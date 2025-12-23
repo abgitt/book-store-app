6d2b5d6d12eebd9b5add815e3832abc63ad47e62
rupa <smrupareddy@gmail.com>
2025-12-23 22:07:38 +0530

    Updating with Retry having basic config

--- order-service/src/main/java/com/sivalabs/bookstore/orders/clients/catalog/ProductServiceClient.java
+++ order-service/src/main/java/com/sivalabs/bookstore/orders/clients/catalog/ProductServiceClient.java
@@ -1,11 +1,15 @@
 package com.sivalabs.bookstore.orders.clients.catalog;
 
+import io.github.resilience4j.retry.annotation.Retry;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
 @Component
 public class ProductServiceClient {
+    private static final Logger log = LoggerFactory.getLogger(ProductServiceClient.class);
 
     private final RestClient restClient;
 
@@ -13,10 +17,12 @@ public class ProductServiceClient {
         this.restClient = restClient;
     }
 
+    @Retry(name = "catalog-service")
     public Optional<Product> getProductByCode(String code) {
         // try{  // uncomment this code to get the correct error code. when catalog service is down, generic exception
         // will thrownhere. But when we are handling with try catch , it will handled by caller ( in our case in
         // validator we are havingInvalidOrderException
+        log.info("Fetching product for code: {}", code);
 
         Product product =
                 restClient.get().uri("/api/products/{code}", code).retrieve().body(Product.class);
